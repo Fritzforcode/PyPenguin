4de7e44dc28903090ef5147d8505f6ae986fab57
Fritzforcode <88397711+Fritzforcode@users.noreply.github.com>
2024-10-24 20:25:18 +0200

    update

--- src/validator.py
+++ src/validator.py
@@ -1,11 +1,11 @@
 import jsonschema
 from jsonschema import validate
 import jsonschema.exceptions
+
 from helper_functions import readJSONFile, ikv, pp
+from validator_constants import opcodeDatabase, projectSchema, allowedOpcodes
 
 ###################################################
-# force stage name
-# force isSprite
 # force sprite only properties
 # force layerOrder: minimum=1 for sprites
 # FORCE 52 by 32 on comments
@@ -18,39 +18,52 @@ from helper_functions import readJSONFile, ikv, pp
 ###################################################
 
 # Function to validate JSON
-def validate_json_schema(json_data, schema):
+def validateJSONSchema(json_data, schema):
     try:
         validate(instance=json_data, schema=schema)
-        return True, "JSON is valid"
+        return None
     except jsonschema.exceptions.ValidationError as err:
         # Custom error message
         error_path = list(err.absolute_path)
         error_message = f"Validation error at {'/'.join(map(str, error_path))}:\n\t{err.message}"
-        return False, error_message
-
-# detects some errors which would not be detected with jsonschema
-def validate_details(data):
-    for sprite in data["sprites"]:
-        for script in sprite["scripts"]:
-            for block in script["blocks"]:
-                opocdeData = list(opcodeDatabase.values())[allowedOpcodes.index(block["opcode"])]
-                pp(opocdeData)
-                #for i,inputKey,inputValue in block["inputs"]:
-                #   
-    return True, "JSON is valid"
-# Example usage
-json_data = readJSONFile("assets/optimized.json")
-is_valid, message = validate_json_schema(json_data, projectSchema)
+        return error_message
 
 
-if is_valid:
-    is_valid, message = True,7#validate_details(data=json_data)
-    if is_valid:
+
+
+# following functions detect some errors causes which would not be detected with jsonschema
+def validateSprite(i, data):
+    if i == 0: # If it should be the stage
+        if data["isStage"] != True:
+            return "'isStage' of the stage (the first sprite) must always be True"
+        if data["name"] != "Stage": 
+            return "'name' of the stage (the first sprite) must always be 'Stage'"
+    else: # If it should be a sprite
+        if data["isStage"] != False:
+            return "'isStage' of a non-stage sprite must always be False"
+    return None
+
+def validateProject(data):
+    for i, sprite in data["sprites"]:
+        error = validateSprite(i=i, data=sprite)
+        if error: return error
+
+    return None
+
+
+
+jsonData = readJSONFile("assets/optimized.json")
+error = validateJSONSchema(jsonData, projectSchema)
+
+
+if not error:
+    error = validateProject(data=jsonData)
+    if not error:
         print("Valid JSON structure")
     else:
-        print(f"Validation failed:\n{message}")
+        print(f"Validation failed:\n{error}")
 else:
-    print(f"Validation failed:\n{message}")
+    print(f"Validation failed:\n{error}")
 
 """
 Validation failed:
