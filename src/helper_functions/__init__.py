597d07bf4613ef7b1cf2604c29475b7c3c3597ca
Fritzforcode <88397711+Fritzforcode@users.noreply.github.com>
2024-10-26 18:33:19 +0200

    renamed
    my file names should all be in snake case

--- src/helper_functions/__init__.py
+++ src/helper_functions/__init__.py
@@ -1,3 +1,3 @@
-from .jsonFiles import readJSONFile, writeJSONFile
-from .tokenLiterals import generateRandomToken, generateSelector
+from .json_files import readJSONFile, writeJSONFile
+from .token_literals import generateRandomToken, generateSelector
 from .other import pp, ikv, flipKeysAndValues, removeStringDuplicates, WhatIsGoingOnError
