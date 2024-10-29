78ae71b0926b5d038f85fe2ad7fc83f15bc4321b
Fritzforcode <88397711+Fritzforcode@users.noreply.github.com>
2024-10-29 21:21:52 +0100

    updated

--- src/validator_constants.py
+++ src/validator_constants.py
@@ -24,16 +24,13 @@ projectSchema = {
   "properties": {
     "sprites": {
       "type"    : "array",
-      #"items"   : { "$ref": "#/definitions/spriteSchema" },
       "minItems": 1
     },
-    "variables": {
+    "globalVariables": {
       "type" : "array",
-      #"items": { "$ref": "#/definitions/variableSchema" }
     },
-    "lists": {
+    "globalLists": {
       "type" : "array",
-      #"items": { "$ref": "#/definitions/listSchema" }
     },
     "tempo": { "type": "integer", "minimum": 20, "maximum": 500 },
     "videoTransparency": { "type": "number" },
@@ -51,8 +48,8 @@ projectSchema = {
   },
   "required": [
     "sprites",
-    "variables",
-    "lists",
+    "globalVariables",
+    "globalLists",
     "extensionData",
     "extensions",
     "meta",
@@ -66,45 +63,23 @@ projectSchema = {
 spriteSchema = {
   "type": "object",
   "properties": {
-    "isStage": { "type": "boolean", "const": False },
-    "name"   : { "type": "string" },
-    "scripts": {
-      "type" : "array",
-#      "items": { "$ref": "#/definitions/scriptSchema" }
-    },
-    "comments": {
-      "type" : "array",
-#      "items": { "$ref": "#/definitions/commentSchema" }
-    },
+    "isStage"       : { "type": "boolean", "const": False },
+    "name"          : { "type": "string", "minLength": 1 },
+    "scripts"       : { "type": "array" },
+    "comments"      : { "type": "array" },
     "currentCostume": { "type": "integer", "minimum": 0 },
-    "costumes": {
-      "type" : "array",
-#      "items": { "$ref": "#/definitions/costumeSchema" }
-    },
-    "sounds": {
-      "type" : "array",
-#      "items": { "$ref": "#/definitions/soundSchema" }
-    },
-    "volume": {
-      "type"   : "number",
-      "minimum": 0,
-      "maximum": 100
-    },
-    "layerOrder": { "type": "integer", "minimum": 1 },
-    "visible"   : { "type": "boolean" },
-    "position"  : {
-      "type"    : "array",
-      "items"   : { "type": "number" },
-      "minItems": 2,
-      "maxItems": 2
-    },
-    "size"         : { "type": "number" },
-    "direction"    : { "type": "number" },
-    "draggable"    : { "type": "boolean" },
-    "rotationStyle": {
-      "type": "string",
-      "enum": ["all around", "left-right", "don't rotate"]
-    }
+    "costumes"      : { "type": "array" },
+    "sounds"        : { "type": "array", },
+    "volume"        : { "type": "number", "minimum": 0, "maximum": 100 },
+    "localVariables": { "type": "array" },
+    "localLists"    : { "type": "array" },
+    "layerOrder"    : { "type": "integer", "minimum": 1 },
+    "visible"       : { "type": "boolean" },
+    "position"      : { "type": "array", "items": {"type": "number"}, "minItems": 2, "maxItems": 2 },
+    "size"          : { "type": "number" },
+    "direction"     : { "type": "number" },
+    "draggable"     : { "type": "boolean" },
+    "rotationStyle" : { "type": "string", "enum": ["all around", "left-right", "don't rotate"]}
   },
   "required": [
     "isStage",
@@ -121,37 +96,23 @@ spriteSchema = {
     "size",
     "direction",
     "draggable",
-    "rotationStyle"
+    "rotationStyle",
+    "localVariables",
+    "localLists"
   ]
 }
 
 stageSchema = {
   "type": "object",
   "properties": {
-    "isStage": { "type": "boolean", "const": True },
-    "name"   : { "type": "string", "const": "Stage" },
-    "scripts": {
-      "type" : "array",
-#      "items": { "$ref": "#/definitions/scriptSchema" }
-    },
-    "comments": {
-      "type" : "array",
-#      "items": { "$ref": "#/definitions/commentSchema" }
-    },
+    "isStage"       : { "type": "boolean", "const": True },
+    "name"          : { "type": "string", "const": "Stage" },
+    "scripts"       : { "type": "array" },
+    "comments"      : { "type": "array" },
     "currentCostume": { "type": "integer", "minimum": 0 },
-    "costumes": {
-      "type" : "array",
-#      "items": { "$ref": "#/definitions/costumeSchema" }
-    },
-    "sounds": {
-      "type" : "array",
-#      "items": { "$ref": "#/definitions/soundSchema" }
-    },
-    "volume": {
-      "type": "number",
-      "minimum": 0,
-      "maximum": 100
-    }
+    "costumes"      : { "type": "array" },
+    "sounds"        : { "type": "array" },
+    "volume"        : { "type": "number", "minimum": 0, "maximum": 100 }
   },
   "required": [
     "isStage",
@@ -168,18 +129,39 @@ stageSchema = {
 scriptSchema = {
   "type": "object",
   "properties": {
-    "position": {
-      "type"    : "array",
-      "items"   : { "type": "integer" },
-      "minItems": 2,
-      "maxItems": 2
-    },
-    "blocks": {
-      "type" : "array",
-#      "items": { "$ref": "#/definitions/blockSchema" }
-    }
+    "position": { "type": "array", "items": {"type": "integer"}, "minItems": 2, "maxItems": 2},
+    "blocks"  : { "type" : "array", "minItems": 1 }
   },
   "required": ["position", "blocks"]
+}
+
+costumeSchema = {
+  "type": "object",
+  "properties": {
+    "name"            : { "type": "string" },
+    "bitmapResolution": { "type": "integer", "minimum": 1 },
+    "dataFormat"      : { "type": "string" },
+    "fileStem"        : { "type": "string" },
+    "rotationCenter": {
+      "type": "array",
+      "items": { "type": ["integer", "number"] },
+      "minItems": 2,
+      "maxItems": 2
+    }
+  },
+  "required": ["name", "dataFormat", "fileStem", "rotationCenter"]
+}
+
+soundSchema = {
+  "type": "object",
+  "properties": {
+    "name"       : { "type": "string" },
+    "dataFormat" : { "type": "string" },
+    "fileStem"   : { "type": "string" },
+    "rate"       : { "type": "integer" },
+    "sampleCount": { "type": "integer" }
+  },
+  "required": ["name", "dataFormat", "fileStem", "rate", "sampleCount"]
 }
 
 blockSchema = {
@@ -243,28 +225,22 @@ inputSchema = {
 variableSchema = {
   "type": "object",
   "properties": {
-    "name"        : { "type": "string" },
-    "currentValue": { "type": ["string", "number"] },
-    "mode"        : { "type": "string", "enum": ["cloud", "global", "local"] },
-    "sprite"      : { "type": ["string", "null"] },
-    "monitor"     : { "type": ["object", "null"] }
+    "name"           : { "type": "string", "minLength": 1 },
+    "currentValue"   : { "type": ["string", "number"] },
+    "monitor"        : { "type": ["object", "null"] },
+    "isCloudVariable": { "type": "boolean" }
   },
-  "required": ["name", "currentValue", "mode", "sprite", "monitor"]
+  "required": ["name", "currentValue", "monitor"]
 }
 
 listSchema = {
   "type": "object",
   "properties": {
     "name"        : { "type": "string" },
-    "currentValue": {
-      "type" : "array",
-      "items": ["string", "number"],
-    },
-    "mode"        : { "type": "string", "enum": ["global", "local"] },
-    "sprite"      : { "type": ["string", "null"] },
+    "currentValue": { "type" : "array", "items": {"type": ["string", "number"]} },
     "monitor"     : { "type": ["object", "null"] }
   },
-  "required": ["name", "currentValue", "mode", "sprite", "monitor"]
+  "required": ["name", "currentValue", "monitor"]
 }
 
 variableMonitorSchema = {
@@ -309,6 +285,7 @@ listMonitorSchema = {
   },
   "required": ["visible", "size", "position"]
 }
+
 
 metaSchema = {
   "type": "object",
