8978357f5f12b9a21275e8c201c795823a980fee
Fritzforcode <88397711+Fritzforcode@users.noreply.github.com>
2024-10-29 21:20:33 +0100

    auto generated

--- src/optimize/variables_lists.py
+++ src/optimize/variables_lists.py
@@ -11,10 +11,8 @@ def translateVariables(data, monitorDatas):
                 mode = "cloud"
             else:
                 mode = "global"
-            sprite = None
         else:
             mode = "local"
-            sprite = data["name"]
         if data["customVars"] != []:
             raise WhatIsGoingOnError("Wow! I have been trying to find out what 'customVars' is used for. Can you explain how you did that? Please contact me on GitHub.")
         
@@ -46,38 +44,34 @@ def translateVariables(data, monitorDatas):
             pass
         newData.append(newVariableData)
     return newData
-    
+
 def translateLists(data, monitorDatas):
     newData = []
-    for spriteData in data:
-        for i,listID,listData in ikv(spriteData["lists"]):
-            name = listData[0]
-            currentValue = listData[1]
-            if spriteData["isStage"]:
-                mode = "global"
-                sprite = None
-            else:
-                mode = "local"
-                sprite = spriteData["name"]
-            
-            monitorIDs = [i["id"] for i in monitorDatas]
-            # if there is no monitor for that variable
-            if listID not in monitorIDs:
-                newMonitorData = None
-            else:
-                monitorData = monitorDatas[monitorIDs.index(listID)]
-                newMonitorData = {
-                    "visible"     : monitorData["visible"],
-                    "size"        : [monitorData["width"], monitorData["height"]],
-                    "position"    : [monitorData["x"], monitorData["y"]],
-                }
-            
-            newListData = {
-                "name"        : name,
-                "currentValue": currentValue,
-                "mode"        : mode,
-                "sprite"      : sprite,
-                "monitor"     : newMonitorData,
+    for i,listID,listData in ikv(data["lists"]):
+        name = listData[0]
+        currentValue = listData[1]
+        if data["isStage"]:
+            mode = "global"
+        else:
+            mode = "local"
+        
+        monitorIDs = [i["id"] for i in monitorDatas]
+        # if there is no monitor for that liat
+        if listID not in monitorIDs:
+            newMonitorData = None
+        else:
+            monitorData = monitorDatas[monitorIDs.index(listID)]
+            newMonitorData = {
+                "visible"     : monitorData["visible"],
+                "size"        : [monitorData["width"], monitorData["height"]],
+                "position"    : [monitorData["x"], monitorData["y"]],
             }
-            newData.append(newListData)
+        
+        newListData = {
+            "name"        : name,
+            "currentValue": currentValue,
+            "monitor"     : newMonitorData,
+        }
+        newData.append(newListData)
     return newData
+    
