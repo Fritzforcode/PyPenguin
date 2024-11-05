c791e2fb9392cced3baad2184bf17ffe24e8965c
Fritzforcode <88397711+Fritzforcode@users.noreply.github.com>
2024-11-05 19:37:51 +0100

    new test

--- 2.py
+++ 2.py
@@ -2,7 +2,7 @@ from src.pypenguin import extractAndOptimizeProject
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src/pypenguin')))
 extractAndOptimizeProject(
-        projectFilePath           = "assets/categories/variables and lists.pmp",
+        projectFilePath           = "assets/studies/varBlockTest.pmp",
         optimizedProjectDirectory = "optimizedProject/",
         temporaryDirectory        = "temporary/",
     )
