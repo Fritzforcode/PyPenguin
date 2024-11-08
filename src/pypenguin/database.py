4484b268b657199c8b3f4be5c8e56acdbd80e8e1
Fritzforcode <88397711+Fritzforcode@users.noreply.github.com>
2024-11-08 19:13:34 +0100

    capitalized categories

--- src/pypenguin/database.py
+++ src/pypenguin/database.py
@@ -6,21 +6,21 @@ opcodeDatabase = {
     # Events
     "event_whenkeypressed": {
         "type": "hat",
-        "category": "events",
+        "category": "Events",
         "newOpcode": "when [KEY_OPTION] key pressed",
         "inputTypes": {},
         "optionTypes": {"KEY_OPTION": "key"}
     },
     "event_whenbroadcastreceived": {
         "type": "hat",
-        "category": "events",
+        "category": "Events",
         "newOpcode": "when i receive [BROADCAST_OPTION]",
         "inputTypes": {},
         "optionTypes": {"BROADCAST_OPTION": "broadcast"}
     },
     "event_broadcast": {
         "type": "instruction",
-        "category": "events",
+        "category": "Events",
         "newOpcode": "broadcast [BROADCAST_INPUT]",
         "inputTypes": {"BROADCAST_INPUT": "broadcast"},
         "optionTypes": {}
@@ -28,14 +28,14 @@ opcodeDatabase = {
     # Control
     "control_if": {
         "type": "instruction",
-        "category": "control",
+        "category": "Control",
         "newOpcode": "if <CONDITION> then {SUBSTACK}",
         "inputTypes": {"CONDITION": "boolean", "SUBSTACK": "script"},
         "optionTypes": {}
     },
     "control_if_else": {
         "type": "instruction",
-        "category": "control",
+        "category": "Control",
         "newOpcode": "if <CONDITION> then {SUBSTACK} else {SUBSTACK2}",
         "inputTypes": {"CONDITION": "boolean", "SUBSTACK": "script", "SUBSTACK2": "script"},
         "optionTypes": {}
@@ -44,35 +44,35 @@ opcodeDatabase = {
     # Operators
     "operator_add": {
         "type": "textReporter",
-        "category": "operators",
+        "category": "Operators",
         "newOpcode": "(NUM1) + (NUM2)",
         "inputTypes": {"NUM1": "number", "NUM2": "number"},
         "optionTypes": {}
     },
     "operator_multiply": {
         "type": "textReporter",
-        "category": "operators",
+        "category": "Operators",
         "newOpcode": "(NUM1) * (NUM2)",
         "inputTypes": {"NUM1": "number", "NUM2": "number"},
         "optionTypes": {}
     },
     "operator_join": {
         "type": "textReporter",
-        "category": "operators",
+        "category": "Operators",
         "newOpcode": "join (STRING1) (STRING2)",
         "inputTypes": {"STRING1": "text", "STRING2": "text"},
         "optionTypes": {}
     },
     "operator_trueBoolean": {
         "type": "booleanReporter",
-        "category": "operators",
+        "category": "Operators",
         "newOpcode": "true",
         "inputTypes": {},
         "optionTypes": {}
     },
     "operator_falseBoolean": {
         "type": "booleanReporter",
-        "category": "operators",
+        "category": "Operators",
         "newOpcode": "false",
         "inputTypes": {},
         "optionTypes": {}
@@ -80,14 +80,14 @@ opcodeDatabase = {
     # Variables
     "data_setvariableto": {
         "type": "instruction",
-        "category": "variables",
+        "category": "Variables",
         "newOpcode": "set [VARIABLE] to (VALUE)",
         "inputTypes": {"VALUE": "text"},
         "optionTypes": {"VARIABLE": "variable"}
     },
     "data_changevariableby": {
         "type": "instruction",
-        "category": "variables",
+        "category": "Variables",
         "newOpcode": "change [VARIABLE] by (VALUE)",
         "inputTypes": {"VALUE": "number"},
         "optionTypes": {"VARIABLE": "variable"}
@@ -95,7 +95,7 @@ opcodeDatabase = {
     # Lists
     "data_addtolist" : {
         "type": "instruction",
-        "category": "lists",
+        "category": "Lists",
         "newOpcode": "add (ITEM) to [LIST]",
         "inputTypes": {"ITEM": "text"},
         "optionTypes": {"LIST": "list"}
@@ -104,28 +104,28 @@ opcodeDatabase = {
     # JSON (jgJSON)
     "jgJSON_getValueFromJSON": {
         "type": "textReporter",
-        "category": "json",
+        "category": "JSON",
         "newOpcode": "get (VALUE) from (JSON)",
         "inputTypes": {"VALUE": "text", "JSON": "text"},
         "optionTypes": {}
     },
     "jgJSON_setValueToKeyInJSON": {
         "type": "textReporter",
-        "category": "json",
+        "category": "JSON",
         "newOpcode": "set (KEY) to (VALUE) in (JSON)",
         "inputTypes": {"KEY": "text", "VALUE": "text", "JSON": "text"},
         "optionTypes": {}
     },
     "jgJSON_json_array_push": {
         "type": "textReporter",
-        "category": "json",
+        "category": "JSON",
         "newOpcode": "in array (array) add (item)",
         "inputTypes": {"array": "text", "item": "text"},
         "optionTypes": {}
     },
     "jgJSON_json_array_get": {
         "type": "textReporter",
-        "category": "json",
+        "category": "JSON",
         "newOpcode": "in array (array) get (index)",
         "inputTypes": {"array": "text", "index": "text"},
         "optionTypes": {}
@@ -133,14 +133,14 @@ opcodeDatabase = {
 # SPECIAL
     "special_variable_value": {
         "type": "textReporter",
-        "category": "variables",
+        "category": "Variables",
         "newOpcode": "VARIABLE",
         "inputTypes": {},
         "optionTypes": {"VARIABLE": "variable"}
     },
     "special_list_value": {
         "type": "textReporter",
-        "category": "lists",
+        "category": "Lists",
         "newOpcode": "LIST",
         "inputTypes": {},
         "optionTypes": {"LIST": "list"}
