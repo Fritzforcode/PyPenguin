a8234daa2192dde4e1b02f480c74342a113cbdf4
Fritzforcode <88397711+Fritzforcode@users.noreply.github.com>
2024-11-13 18:04:28 +0100

    small input fix

--- src/extractionTest.py
+++ src/extractionTest.py
@@ -1,9 +1,9 @@
 from pypenguin import extractAndOptimizeProject, deoptimizeAndCompressProject, validateProject
 from pypenguin.helper_functions import readJSONFile, pp
 optimizedData = extractAndOptimizeProject(
-    projectFilePath           = "../assets/studies/switches.pmp",
-    optimizedProjectDirectory = "../extractedProject",
-    temporaryDirectory        = "../temporary",
+    projectFilePath           = "assets/studies/switches.pmp",
+    optimizedProjectDirectory = "extractedProject",
+    temporaryDirectory        = "temporary",
     writeDebugFiles           = True,
 )
 pp(optimizedData)
