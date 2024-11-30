301d1f94ca0515657c3a02941652b8ab73620bbf
Fritzforcode <88397711+Fritzforcode@users.noreply.github.com>
2024-11-30 22:08:09 +0100

    completed file reference transition

--- src/pypenguin/helper_functions/json_files.py
+++ src/pypenguin/helper_functions/json_files.py
@@ -11,39 +11,14 @@ def readJSONFile(filePath):
     #for frame in stack[1:]:
     #    filename = frame.filename
     #    print("-", filename)
-    def read(filePath):
-        with open(filePath, "r", encoding="utf-8") as file:
-            string = file.read()
-        return parser.loads(string)
-    try:
-        return read(filePath)
-    except FileNotFoundError:
-        try:
-            filePath = "../" + filePath # Go up a directory
-            return read(filePath)
-        except FileNotFoundError:
-            filePath = "../" + filePath # Go up a second directory
-            return read(filePath)    
+    with open(filePath, "r", encoding="utf-8") as file:
+        string = file.read()
+    return parser.loads(string) 
 
 def writeJSONFile(filePath, data, beautiful:bool=True):
     print("write", filePath)
-    def write(filePath, data):
-        with open(filePath, "w") as file:
-            if beautiful:
-                dump(data, file, indent=4)
-            else:
-                dump(data, file)
-    try:
-        return write(filePath=filePath, data=data)
-    except FileNotFoundError:
-        try:
-            filePath = "../" + filePath # Go up a directory
-            return write(filePath=filePath, data=data)
-        except FileNotFoundError:
-            try:
-                filePath = "../" + filePath # Go up a second directory
-                return write(filePath=filePath, data=data)
-            except FileNotFoundError:
-                filePath = "../" + filePath # Go up a third directory
-                return write(filePath=filePath, data=data)
-
+    with open(filePath, "w") as file:
+        if beautiful:
+            dump(data, file, indent=4)
+        else:
+            dump(data, file)
